af7a1b52a5b9cb20a41a4fc2a60543f93873e9ea
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-01 15:02:18 -0800

    add jQuery code to grab text element, count length within it, and update counter element with current number of characters remaining from 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,14 +2,16 @@
 
 $(document).ready(function() {
 
-  
-  
+  // grabbing text element
   const $text = $('#tweet-text');
 
+  // setting up event listener to listen for input on text box
   $text.on('input', function() {
-    console.log(140 - this.textLength);
-    
+
+    // grabbing text length within textbox, grabbing counter element from DOM tree, reassigning value of the counter element to text length
+    $textLength =  $(this).val().length
+    $counter = $($(this).parent()[0][2]);
+     $counter.val(140 - $textLength)
+
   });
-
-
 });
