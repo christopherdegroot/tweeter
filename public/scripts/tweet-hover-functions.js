584272a806644386ffbf99ee67795ecfa5c1f4db
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-01 18:35:00 -0800

    Write jQuery code for icon color change when mouseover

--- public/scripts/tweet-hover-functions.js
+++ public/scripts/tweet-hover-functions.js
@@ -1,9 +1,8 @@
 
 $(document).ready(function() {
  
-  
+  // jQuery code for box shadow on article (tweet boxes) boxes on mouseover
 const $tweetArticle = $('article')
-
 
 $tweetArticle.on('mouseenter', function() {
   $tweetArticle.addClass("box-shadow")
@@ -14,5 +13,15 @@ $tweetArticle.on('mouseleave', function() {
 });
 
 
+// jQuery code for icon mouse-over to change color of individual icons
+const $tweetIcons = $('i')
+
+$tweetIcons.on('mouseenter', function() {
+  $(this).addClass("tweet-text-color")
+})
+
+$tweetIcons.on('mouseleave', function() {
+  $(this).removeClass("tweet-text-color")
+})
 
 });
