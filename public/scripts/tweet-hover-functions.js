acae359ea5c13afd8d406200b3d6137f7d7811b1
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-01 18:19:14 -0800

    add jQuery code to select tweet boxes and apply mouseover box-shadow effect while hovering, and remove when mouse leaves box

--- public/scripts/tweet-hover-functions.js
+++ public/scripts/tweet-hover-functions.js
@@ -5,47 +5,14 @@ $(document).ready(function() {
 const $tweetArticle = $('article')
 
 
-$tweetArticle.on('mouseover', function() {
-  console.log('im hovered on')
+$tweetArticle.on('mouseenter', function() {
   $tweetArticle.addClass("box-shadow")
 });
 
+$tweetArticle.on('mouseleave', function() {
+  $tweetArticle.removeClass("box-shadow")
 });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready(function() {
-
-//   // grabbing text element
-//   const $text = $('#tweet-text');
-
-//   // setting up event listener to listen for input on text box
-//   $text.on('input', function() {
-
-//     // grabbing text length within textbox, grabbing counter element from DOM tree, reassigning value of the counter element to text length
-//     $textLength =  $(this).val().length;
-
-//     $counter = $($(this).parent()[0][2]);
-//     $counter.val(140 - $textLength);
-
-//     // adding if statements to control the colour if the counter is currently above or below 0 (if below, red)
-//     if (140 - $textLength < 0) {
-//       $counter.attr("id", "container2")
-//     }
-//     if (140 - $textLength >= 0) {
-//       $counter.attr("id", "container3")
-//     }
-//   });
-// });
+});
