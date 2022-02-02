7507df902bc93fc58946bb64228bb25ec2fae308
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-02 15:59:45 -0800

    add createTweetElement function, add renderTweet function to make use of createTweetElement function to population live site with the new database of tweets after clearing the old database from the DOM

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,81 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready( function() {
+
+  // let tweetData =  [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
+
+  
+
+  const createTweetElement = function(tweetData) {
+    const $tweetSection = $('.tweets');
+    console.log('logging tweetData', tweetData)
+
+    return `<article>
+    <header>
+      <p> <img style="padding-right: 10px" src="${tweetData.user.avatars}"> ${tweetData.user.name}</p>
+      <p style="color: #acc5c9; font-weight: 600;">${tweetData.user.handle}</p>
+    </header>
+    <div class="tweet-text">
+      <p>${tweetData.content.text}</p>
+    </div>
+    <footer>
+      <p><b>${timeago.format(tweetData.created_at)}</b></p>
+      <div>  
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i> 
+      </div>
+    </footer>
+  </article>`
+  };
+
+  // const $tweet = createTweetElement(tweetData)
+  // $tweetSection.append($tweet);
+
+
+  const renderTweets = function() {
+    const $tweetSection = $('.tweets');
+   
+
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: (tweets) => {
+        $tweetSection.empty();
+        for (const tweet in tweets) {
+          console.log('logging tweet and tweets in for loop', tweet, tweets);
+          const $tweet = createTweetElement(tweets[tweet]);
+          $tweetSection.append($tweet);
+        }
+      }
+    })
+
+  };
+
+  renderTweets();
+
+});
