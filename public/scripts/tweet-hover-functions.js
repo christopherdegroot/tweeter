6119926d2b642739d7041ba0c29dda751376a529
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-02-02 16:00:24 -0800

    added event listener for ajaxComplete() in order to preserve event listeners/CSS functionality on newly created DOM elements from ajax request

--- public/scripts/tweet-hover-functions.js
+++ public/scripts/tweet-hover-functions.js
@@ -1,27 +1,35 @@
-
-$(document).ready(function() {
+  $(document).ready(function() {
+     $(document).ajaxComplete(function() {
  
-  // jQuery code for box shadow on article (tweet boxes) boxes on mouseover
-const $tweetArticle = $('article')
-
-$tweetArticle.on('mouseenter', function() {
-  $tweetArticle.addClass("box-shadow")
+    // jQuery code for box shadow on article (tweet boxes) boxes on mouseover
+ 
+  const $tweetContainer = $('article')
+  
+  
+    $tweetContainer.on('mouseenter', function() {
+      console.log('im bein hovered on')
+      $(this).addClass("box-shadow")
+    });
+    
+    $tweetContainer.on('mouseleave', function() {
+      $(this).removeClass("box-shadow")
+    });
+  
+  
+  
+  
+  // jQuery code for icon mouse-over to change color of individual icons
+  const $tweetIcons = $('i')
+  
+  $tweetIcons.on('mouseenter', function() {
+    $(this).addClass("tweet-text-color")
+  })
+  
+  $tweetIcons.on('mouseleave', function() {
+    $(this).removeClass("tweet-text-color")
+  })
+  
+   });
+  
 });
-
-$tweetArticle.on('mouseleave', function() {
-  $tweetArticle.removeClass("box-shadow")
-});
-
-
-// jQuery code for icon mouse-over to change color of individual icons
-const $tweetIcons = $('i')
-
-$tweetIcons.on('mouseenter', function() {
-  $(this).addClass("tweet-text-color")
-})
-
-$tweetIcons.on('mouseleave', function() {
-  $(this).removeClass("tweet-text-color")
-})
-
-});
+  
